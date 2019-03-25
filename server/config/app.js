d5dc31e5ded670610d186e9a24e05e5acfee8418
Abirami <abiramirobertkennedy@gmail.com>
2019-03-25 07:20:38 -0700

    Updated json

--- server/config/app.js
+++ server/config/app.js
@@ -4,10 +4,15 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-
+let cors = require('cors');
 // modules for authentication
 let session = require('express-session');
 let passport = require('passport');
+let passportJWT = require('passport-jwt');
+let JWTstrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+
 let passportLocal = require('passport-local');
 let localStrategy = passportLocal.Strategy;
 let flash = require('connect-flash');
@@ -42,6 +47,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+app.use(cors());
+
 // setup express-session
 app.use(session({
   secret: "SomeSecret",
@@ -69,9 +76,29 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//this part verifies that the token is sent by the user and is valid
+let jwtOptions={};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = DB.secret;
 
-app.use('/', indexRouter);
-app.use('/contact-list', contactRouter);
+
+let strategy = new JWTstrategy(jwtOptions, (jwt_payload,done) => {
+  User.findById(jwt_payload.id)
+  .then(user => {
+    return done(null,user);
+  })
+  .catch(err => {
+    return done(err, false);
+  });
+});
+
+passport.use(strategy);
+
+app.use('/api', indexRouter);
+app.use('/api/contact-list', contactRouter); //Protect this section
+
+//capture random or incorrect URL information
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -12,21 +12,13 @@ module.exports.displayContactList = (req, res, next) =>{
         else {
            // console.log(contactList);
 
-            res.render('contacts/index', {
-                title: 'Contact List',
-                contactList: contactList,
-                displayName: req.user ? req.user.displayName : ""
-            });
-            
+           return res.json({success:true, msg: 'Contact list displayed successfully', contactList: contactList, user: req.user});
         }
     });
 }
 
 module.exports.displayAddPage = (req, res, next) => {
-    res.render('contacts/add', {
-        title: 'Add New Contact',
-        displayName: req.user ? req.user.displayName : ""
-    });
+    return res.json({success:true, msg: 'successfully displayed add page'});
 }
 
 module.exports.processAddPage = (req, res, next) => {
@@ -44,7 +36,7 @@ module.exports.processAddPage = (req, res, next) => {
         }
         else {
             // refresh the contact list
-            res.redirect('/contact-list');
+            res.json({success:true, msg: 'successfully added new contact'});
         }
     });
 }
@@ -60,11 +52,7 @@ module.exports.displayEditPage = (req, res, next) => {
         else
         {
             // show the edit view
-            res.render('contacts/edit', {
-                title: 'Edit Contact',
-                contact: contactObject,
-                displayName: req.user ? req.user.displayName : ""
-            });
+            res.json({success:true, msg: 'successfully displayed edit page',contact:contactObject});
         }
     });
 }
@@ -86,7 +74,7 @@ module.exports.processEditPage = (req, res, next) => {
         }
         else {
             // refresh the contact list
-            res.redirect('/contact-list');
+            res.json({success:true, msg: 'successfully edited contact page', contact:updatedContact});
         }
     })
 }
@@ -101,7 +89,7 @@ module.exports.performDelete = (req, res, next) => {
         }
         else {
             // refresh the contact list
-            res.redirect('/contact-list');
+            res.json({success:true, msg: 'successfully deleted contact'});
         }
     });
 }
